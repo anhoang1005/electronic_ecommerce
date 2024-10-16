31ddc284bc258755d434bb41297285a4ac3856f6
Mup002 <116329105+Mup002@users.noreply.github.com>
2024-10-16 13:16:33 +0700

    Delete src/main/java/com/example/ecommerce/repository/ODetailsRepo.java

--- src/main/java/com/example/ecommerce/repository/ODetailsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.entities.Order_Detail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ODetailsRepo extends JpaRepository<Order_Detail,Long> {
-}
