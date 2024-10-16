a1c38e6bb8df532b7caf5e1ec50b608a51bcfe2c
Mup002 <116329105+Mup002@users.noreply.github.com>
2024-10-16 13:16:09 +0700

    Delete src/main/java/com/example/ecommerce/repository/OItemRepo.java

--- src/main/java/com/example/ecommerce/repository/OItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.entities.Order_Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OItemRepo extends JpaRepository<Order_Item,Long> {
-}
