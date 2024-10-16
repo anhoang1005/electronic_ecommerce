2233ea6422c6835908bb1331ac037fa9a157b924
Mup002 <116329105+Mup002@users.noreply.github.com>
2024-10-16 13:16:58 +0700

    Delete src/main/java/com/example/ecommerce/repository/PCategoryRepo.java

--- src/main/java/com/example/ecommerce/repository/PCategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce.repository;
-
-import com.example.ecommerce.entities.Product_Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PCategoryRepo extends JpaRepository<Product_Category,Long> {
-}
